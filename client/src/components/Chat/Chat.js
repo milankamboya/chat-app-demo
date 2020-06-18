5f27b8da41b7127f47eea50eafba1311a1ea0189
Milan <kamboyamilan@gmail.com>
2020-06-18 12:37:39 +0530

    get name and room in chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
+import queryString from 'query-string';
 
 const Chat = ({ location }) => {
+
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(room);
+  }, []);
+
   return (
     <div>
-      {location.search}
+      {name}-{room}
     </div>
   )
 }
